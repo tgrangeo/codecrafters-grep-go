f5588efc0bfc41268caffa75b5f246a6dece72c3
tgrangeo <thomas.grangeon9@gmail.com>
2024-08-30 10:01:50 +0200

    match negative char group

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -9,7 +9,6 @@ import (
 	// "unicode/utf8"
 )
 
-// Usage: echo <input_text> | your_program.sh -E <pattern>
 func main() {
 	if len(os.Args) < 3 || os.Args[1] != "-E" {
 		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
@@ -18,7 +17,7 @@ func main() {
 
 	pattern := os.Args[2]
 
-	line, err := io.ReadAll(os.Stdin) // assume we're only dealing with a single line
+	line, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
 		os.Exit(2)
@@ -33,15 +32,26 @@ func main() {
 	if !ok {
 		os.Exit(1)
 	}
-
-	// default exit code is 0 which means success
 }
 
 func matchLine(line []byte, pattern string) (bool, error) {
 	// if utf8.RuneCountInString(pattern) != 1 {
 	// 	return false, fmt.Errorf("unsupported pattern: %q", pattern)
 	// }
-	if bytes.ContainsAny(line, pattern) {
+	if regexp.MustCompile(`^\[\^[a-zA-Z0-9]+\]$`).MatchString(pattern) {
+		re := regexp.MustCompile(`^\[\^([a-zA-Z0-9]+)\]$`)
+		matches := re.FindSubmatch([]byte(pattern))
+		if len(matches) > 1 {
+			toFind := string(matches[1])
+			for _, char := range toFind {
+
+				if bytes.ContainsRune(line, char) {
+					return false, nil
+				}
+			}
+			return true, nil
+		}
+	} else if bytes.ContainsAny(line, pattern) {
 		return true, nil
 	} else if pattern == `\d` {
 		if bytes.ContainsAny(line, "0123456789") {
@@ -51,15 +61,6 @@ func matchLine(line []byte, pattern string) (bool, error) {
 		re := regexp.MustCompile(`[a-zA-Z0-9]`)
 		if re.Match(line) {
 			return true, nil
-		}
-	} else if regexp.MustCompile(`^\[\^[a-zA-Z0-9]+\]$`).MatchString(pattern){
-		re := regexp.MustCompile(`^\[\^[a-zA-Z0-9]+\]$`)
-		matches := re.FindSubmatch([]byte(pattern))
-		if len(matches) > 1 {
-			toFind :=  string(matches[1])
-			if !bytes.ContainsAny(line, toFind) {
-				return true, nil
-			}
 		}
 	}
 	return false, nil

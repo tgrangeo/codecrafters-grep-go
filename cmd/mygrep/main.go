e0b46813ebe85358d12022e5ca1574d0fc8d4590
tgrangeo <thomas.grangeon9@gmail.com>
2024-08-31 16:47:16 +0200

    back references

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
@@ -28,7 +29,27 @@ func main() {
 	}
 }
 
+func checkBackReferences(line []byte, pattern string) (string, error) {
+	re, _ := regexp.Compile("\\1")
+	if ( re.Match(line)){
+		res := pattern
+		begin := strings.IndexRune(pattern, '(')
+		end := strings.IndexRune(pattern, ')')
+		group := pattern[begin:end]
+		res = strings.ReplaceAll(pattern, "(", "")
+		res = strings.ReplaceAll(pattern, ")", "")
+		res = strings.ReplaceAll(pattern, "\\1", group)
+		return res, nil
+	}
+	return pattern, nil
+}
+
 func matchLine(line []byte, pattern string) (bool, error) {
+	pattern, err := checkBackReferences(line,pattern)
+	fmt.Println(pattern)
+	if err != nil {
+		return false, fmt.Errorf("invalid back reference: %v", err)
+	}
 	re, err := regexp.Compile(pattern)
 	if err != nil {
 		return false, fmt.Errorf("invalid pattern: %v", err)

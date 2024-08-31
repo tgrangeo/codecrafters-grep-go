51f0453c8ed219801abff24ef8200f42fe49c8f3
tgrangeo <thomas.grangeon9@gmail.com>
2024-08-31 08:41:02 +0200

    refacto

--- cmd/mygrep/main.go
+++ cmd/mygrep/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -13,71 +12,26 @@ func main() {
 		fmt.Fprintf(os.Stderr, "usage: mygrep -E <pattern>\n")
 		os.Exit(2)
 	}
-
 	pattern := os.Args[2]
-
 	line, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: read input text: %v\n", err)
 		os.Exit(2)
 	}
-
 	ok, err := matchLine(line, pattern)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(2)
 	}
-
 	if !ok {
 		os.Exit(1)
 	}
 }
 
-func getPattern(patternLine string) []string {
-	res := []string{}
-	for i := 0; i < len(patternLine); i++ {
-		if patternLine[i] == '\\' && i+1 < len(patternLine) {
-			res = append(res, patternLine[i:i+2])
-			i++
-		} else {
-			res = append(res, string(patternLine[i]))
-		}
-	}
-	return res
-}
-
-func checkNextPattern(c byte, p string) bool {
-	switch p {
-	case `\d`:
-		return bytes.ContainsAny([]byte{c}, "0123456789")
-	case `\w`:
-		return regexp.MustCompile(`[a-zA-Z0-9]`).Match([]byte{c})
-	default:
-		if p[0] == '[' && p[len(p)-1] == ']' {
-			if p[1] == '^' {
-				return !bytes.ContainsAny([]byte{c}, p[2:len(p)-1])
-			}
-			return bytes.ContainsAny([]byte{c}, p[1:len(p)-1])
-		}
-		return string(c) == p
-	}
-}
-
 func matchLine(line []byte, pattern string) (bool, error) {
-	patternArray := getPattern(pattern)
-	lineIndex := 0
-
-	for _, p := range patternArray {
-		if lineIndex >= len(line) {
-			return false, nil
-		}
-
-		if !checkNextPattern(line[lineIndex], p) {
-			return false, nil
-		}
-
-		lineIndex++
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return false, fmt.Errorf("invalid pattern: %v", err)
 	}
-
-	return true, nil
+	return re.Match(line), nil
 }
